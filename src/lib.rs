60bfe1c35ab83264dc8f953293de5dfb6f9bf3ec
unyime1 <lordunyime@gmail.com>
2022-01-29 05:54:48 +0100

    added file searching

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,10 @@ use std::fs;
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // Read file.
     let contents = fs::read_to_string(config.filename)?;
-    println!("{}", contents);
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
@@ -25,5 +28,34 @@ impl Config {
         let filename = args[2].clone();
 
         Ok(Config { query, filename })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    // handle character search.
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+        assert_ne!(vec!["Pick three."], search(query, contents))
     }
 }
