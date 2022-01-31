40cc7529aff54bc412f07e207a3883a67b24dc42
unyime1 <lordunyime@gmail.com>
2022-01-31 17:15:55 +0100

    introduced iteratiors to args and search

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,38 @@
+use std::env;
 use std::error::Error;
 use std::fs;
-use std::env;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        // parse and return configuration arguments
+
+        // discard first arg since it returns name of program which is not useful
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
+    }
+}
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // Read file.
@@ -19,60 +51,23 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub struct Config {
-    pub query: String,
-    pub filename: String,
-    pub case_sensitive: bool,
-}
-
-
-impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        // parse and return configuration arguments
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
-
-        Ok(Config { query, filename, case_sensitive })
-    }
-}
-
-
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     // handle case sensitive character search.
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
-
-pub fn search_case_insensitive<'a>(
-    query: &str,
-    contents: &'a str,
-) -> Vec<&'a str> {
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     // perform case insensitive search.
 
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
-
 
 #[cfg(test)]
 mod tests {
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,7 @@ use std::env;
 use std::process;
 
 fn main() {
-    // Collect command line arguments from terminal.
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("problem parsing arguments:{}", err);
         process::exit(1);
     });
